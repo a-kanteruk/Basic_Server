4528d675c3a0d48afeb2b0c04d02a7a1a357df8e
a-kanteruk <a.kanteruk@tg.dunice.net>
2023-04-11 09:21:34 +0300

    First commit

---
+++ src/test/java/net/dunice/Basic_server/BasicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dunice.Basic_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
